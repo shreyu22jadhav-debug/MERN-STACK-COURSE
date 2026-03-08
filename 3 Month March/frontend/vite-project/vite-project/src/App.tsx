38f1643d280a6067ba70b6349dd7fd3f9c8f7497
https://github.com/shreyu22jadhav-debug <shreyu22jadhav@gmail.com>
2026-03-08 13:16:27 +0530

    App.tsx

---
+++ 3 Month March/frontend/vite-project/vite-project/src/App.tsx
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+function App() {
+  
+
+  return (
+    <>
+    <h2 className='text-danger text-center my-5'>CRUD-MERN STACK PROJECT START</h2>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6'>
+          <h3 className='border text-center'>Add Item</h3>
+          </div>
+          <div className='col-md-6'>
+            <h3 className='border text-center'>Item List</h3>
+        </div>
+      </div>
+    </div>
+      
+    </>
+  );
+}
+
+export default App
