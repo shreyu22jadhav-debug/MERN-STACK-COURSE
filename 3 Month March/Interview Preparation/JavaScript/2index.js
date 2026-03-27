9e874b5bf74a95f61f7cbd2d55f2be4727f66f25
https://github.com/shreyu22jadhav-debug <shreyu22jadhav@gmail.com>
2026-03-27 15:24:39 +0530

    Synchronous and Asychronous

---
+++ 3 Month March/Interview Preparation/JavaScript/2index.js
@@ -0,0 +1,53 @@
+//javascript
+//React.js
+//node.js
+
+// Synchronous and Asychronous
+//callbacks
+//promises
+// asyn/swait
+
+//1.set Timeout
+
+//console.log(1)
+//console.log(11)
+//setTimeout(() => {
+  //  console.log(2)
+//},1000)
+
+//console.log(3)
+
+//1.call backs
+
+const getData = (callbacksfunction) => {
+    console.log("Hello Get Data")
+    callbacksfunction()
+}
+
+ // const callbacksfunction =() => {
+   // console.log("call back function called")
+
+ // getData(callbacksfunction)
+
+ getData(()=> console.log("122121"))
+
+
+ // 2.promises -asynchronous
+ // resolve /pending /reject
+
+ const mypromises = new Promise((resolve,reject) => {
+    const error = false 
+    if(error == true){
+        resolve("promise resolved")
+    }else{
+        reject("promise reject")
+    }})
+    mypromises.then((res) => console.log(res)).catch((error) => console.log(error))
+    
+// 3 .async /await
+const mypromise = async () => {
+    //APT integration
+    //DB call -add delete update  get from DB -await
+}
+
+ 
